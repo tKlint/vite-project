7e08e3b368dd03136d4d7e858312e77c90167fbb
qikun <qikun@Qikun17deMacBook-Pro.local>
2022-06-30 17:26:22 +0800

    refactor: 删除无用的代理配置

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const proxy = require("http-proxy-middleware");
-module.exports = function (app) {
-  app.use(
-    proxy.createProxyMiddleware("/api", {
-      target: " http://192.168.1.57:9527",//
-      changeOrigin: true
-    })
-  );
-};
